2275b8b60c9e2c20850c865307ee207ca1a80137
Jason <jasonblevins@gmail.com>
2015-08-09 21:02:07 -0700

    new

--- JayLinkedList/src/net/jasonblevins/academic/JayLinkedList/Demo.java
+++ JayLinkedList/src/net/jasonblevins/academic/JayLinkedList/Demo.java
@@ -2,5 +2,12 @@ package net.jasonblevins.academic.JayLinkedList;
 
 public class Demo {
 	
+	public static void main(String[] args){
+		
+		LinkedList<Integer> myList = new LinkedList<Integer>();
+		
+		myList.add(5);
+	}
+	
 
 }
---
+++ JayLinkedList/src/net/jasonblevins/academic/JayLinkedList/LinkedList.java
@@ -0,0 +1,20 @@
+package net.jasonblevins.academic.JayLinkedList;
+
+public class LinkedList<T> {
+	public Node<T> head;
+	public int size;
+	
+	public LinkedList(){
+		head = null;
+		size = 0;
+		
+	}
+	
+	public void add(T data){
+		
+		
+	}
+	
+	
+
+}
