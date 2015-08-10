20a437aa56f0de002f76bbae218859bb0aeeae0f
Jason <jasonblevins@gmail.com>
2015-08-09 17:02:15 -0700

    Work

---
+++ LinkedList/src/net/jasonblevins/academic/LinkedList/JLinkedList.java
@@ -0,0 +1,22 @@
+package net.jasonblevins.academic.LinkedList;
+
+public class JLinkedList<T> {
+	
+	
+	public JNode<T> head;
+	public int size;
+	
+	
+	public JLinkedList(){
+		head = null;
+		size = 0;
+	}
+	
+	public void add(T data){
+		
+		
+	}
+	
+	
+
+}
--- LinkedList/src/net/jasonblevins/academic/LinkedList/LinkedList.java
+++
@@ -1,9 +0,0 @@
-package net.jasonblevins.academic.LinkedList;
-
-public class LinkedList {
-	//update!
-	//update 2.0
-	//update 3.0
-	
-
-}
