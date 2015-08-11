757e9753b90b83ee861398fcb8284fb4336e3fbf
Jason <jasonblevins@gmail.com>
2015-08-11 08:01:25 -0700

    Added new package

---
+++ JayLinkedList/src/net/jasonblevins/academic/LinkedListOperator/LinkedListOperator.java
@@ -0,0 +1,13 @@
+package net.jasonblevins.academic.LinkedListOperator;
+
+import net.jasonblevins.academic.LinkedList.LinkedList;
+
+public class LinkedListOperator {
+
+	static <T> LinkedList<T> mergeTwoSortedLists(LinkedList<T> linkedListA, LinkedList<T> linkedListB) {
+		LinkedList<T> mergedList = new LinkedList<T>();
+
+		return mergedList;
+
+	}
+}
